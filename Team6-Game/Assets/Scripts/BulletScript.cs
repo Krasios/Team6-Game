63a2e226f41a939a0514f6ed3f10665d537ed5b2
Krasios <sharonkuang97@gmail.com>
2019-01-23 21:00:41 -0800

    Basic shooting mechanism

---
+++ Team6-Game/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 6;
+
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0.0f,speed);
+    }
+
+    // Update is called once per frame
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- Team6-Game/Assets/Scripts/PlayerCtrl.cs
+++ Team6-Game/Assets/Scripts/PlayerCtrl.cs
@@ -7,11 +7,15 @@ public class PlayerCtrl : MonoBehaviour
 {
     public float speed;
     public string name;
+    public GameObject bulletPrefab;
 
+    private Light playerLight;
     private Rigidbody2D rigidB;
+
 
     void Start() {
         rigidB = GetComponent<Rigidbody2D>();
+        playerLight = GetComponent<Light>();
     }
 
     void FixedUpdate() {
@@ -19,6 +23,12 @@ public class PlayerCtrl : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical_"+name);
 
         rigidB.AddForce(new Vector2(moveHorizontal,moveVertical)*speed);
+        if (playerLight.range > 5){
+            playerLight.range -= 0.01f;
+        }
+        if (Input.GetButton("Jump")) {
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
     }
     void OnTriggerEnter2D(Collider2D other) {
 
