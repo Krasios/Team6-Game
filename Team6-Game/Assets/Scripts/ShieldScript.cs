040e0bd71fd7a64a06f728b326e6620029c1c9ed
Krasios <sharonkuang97@gmail.com>
2019-01-31 10:54:25 -0800

    AOE damage on shield regen

--- Team6-Game/Assets/Scripts/ShieldScript.cs
+++ Team6-Game/Assets/Scripts/ShieldScript.cs
@@ -17,8 +17,7 @@ public class ShieldScript : MonoBehaviour
     private bool shieldActive;
     private float timeRegen;
 
-    void Start()
-    {
+    void Start() {
         shield = Instantiate(shieldPrefab, player.transform.position, Quaternion.identity);        
         shield.transform.parent = player.transform;
         shield.transform.position = player.transform.position;
@@ -27,9 +26,7 @@ public class ShieldScript : MonoBehaviour
         distance = Vector2.Distance(player.transform.position,partner.transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         distance = Vector2.Distance(player.transform.position,partner.transform.position);
         float shieldRadius = (distance > 20) ? (maxShieldDistance/distance) : maxShieldRadius;
         shield.GetComponent<CircleCollider2D>().radius = shieldRadius*2.5f/(shield.transform.localScale.x);
@@ -39,18 +36,27 @@ public class ShieldScript : MonoBehaviour
             if (timeRegen >= distance) {
                 shieldActive = true;
                 timeRegen = 0;
+                //AOE on shield regeneration
+                GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                for (int i=0; i<enemies.Length; i++) {
+                    if( Vector2.Distance(player.transform.position, enemies[i].transform.position ) <= shieldRadius*3f) {
+                        enemies[i].SetActive(false);
+                    }
+                }
+                shield.GetComponent<SpriteRenderer>().enabled = true;               
             }
         }
     }
+
     void OnTriggerEnter2D(Collider2D other) {
         if (shieldActive && other.gameObject.CompareTag("Enemy")) {
-            Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
             hitsTaken += 1;
             if (hitsTaken > maxHits) {
                 shieldActive = false;
                 hitsTaken = 0;
+                shield.GetComponent<SpriteRenderer>().enabled = false;
             }
         }
-
     }
 }
