b64001d77b6e27a96c56be4bc52fb1c10a554028
Con <connsteele96@gmail.com>
2019-02-26 10:35:10 -0800

    Added mothership
    If a player comes into contact with the entry of the mothership (and they have more than 0 light) then their light is taken and added to the mothership. Right now it just prints out to the debug log.

---
+++ Team6-Game/Assets/Scripts/MothershipCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MothershipCtrl : MonoBehaviour
+{
+    public float mothershipLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start with no light
+        mothershipLight = 0.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<PlayerCtrl>().lightCount > 0)
+            {
+                mothershipLight += other.gameObject.GetComponent<PlayerCtrl>().lightCount;
+                Debug.Log("Mothership's Current Light: " + mothershipLight.ToString());
+                other.gameObject.GetComponent<PlayerCtrl>().lightCount = 0;
+                other.gameObject.GetComponent<PlayerCtrl>().updateLightText();
+            }
+        }
+    }
+}
