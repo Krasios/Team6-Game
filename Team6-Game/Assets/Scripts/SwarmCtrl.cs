93fcdb001369c8d4c8fc2673408ddda0a6c396b3
Krasios <sharonkuang97@gmail.com>
2019-03-12 21:01:48 -0700

    bonus level added

---
+++ Team6-Game/Assets/Scripts/SwarmCtrl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmCtrl : MonoBehaviour {
+
+    // adapted from https://processing.org/examples/flocking.html
+
+    public int numberOfAliens = 100;
+    public float spawnLocationX = 300;
+    public float spawnLocationY = 300;
+    public float areaWidth = 990;
+    public float areaHeight = 540;
+    public float alienSize = 5f;
+    public float maxSpeed = 2f;
+    public float maxForce = 0.1f;
+    public float separationWeight = 2.5f;
+    public float alignmentWeight = 1;
+    public float cohesionWeight = 1;
+    public float avoidPlayerWeight = 2f;
+    public GameObject swarmMember;
+    public GameObject player;
+
+    Flock flock;
+
+    void Start () {
+        flock = new Flock();
+        for ( int i = 0; i < numberOfAliens; i++ ) {
+            var alien = new Boid(spawnLocationX+Random.Range(-2,2), spawnLocationY+Random.Range(-2,2), this);
+            var alienObject = Instantiate(swarmMember);
+            alienObject.SetActive(true);
+            alienObject.GetComponent<memberMovement>().alienBoid = alien;
+            flock.AddBoid(alien);
+        }
+    }
+    
+    void Update () {
+        flock.Update();
+    }
+
+}
---
+++ Team6-Game/Assets/Scripts/memberMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class memberMovement : MonoBehaviour {
+    
+    public float timeOffset = 0;
+    public float radius = 50;
+    public float speed = 1;
+    public Boid alienBoid;
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector2(alienBoid.location.x, alienBoid.location.y);
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, new Vector3( alienBoid.velocity.x, alienBoid.velocity.y, 0));
+    }
+}
